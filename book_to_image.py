a5ff6950f4429487268468c650a69ad3aa05de72
Aaron Marriner <amarriner@amarriner.com>
2014-09-24 19:33:18 +0000

    Working scripts to encrypt and decrypt book

--- book_to_image.py
+++ book_to_image.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 """Convert novels/text to images...somehow"""
 
+import codecs
 import gd
 import sys
 
 
+# Initial dimensions of temp image
 DIMENSIONS = (500, 5000)
 
 
@@ -23,7 +25,8 @@ def increment_coords(coords):
 def load_text():
    """Load some text from somewhere for use in processing"""
 
-   f = open('little_women.txt')
+   # Read file in as UTF-8
+   f = codecs.open('little_women.txt', 'r', encoding='utf-8')
    text = f.read()
    f.close()
 
@@ -37,19 +40,33 @@ def set_pixels_by_text(image):
    coords = [0, 0]
    rgb = [0, 0, 0]
 
+   # Get text from somewhere
    text = load_text()
 
    for c in text:
 
-      rgb[i % 3] = ord(c)
-
+      # Every third character write out as a pixel where
+      # RGB colors are the three previous character's
+      # unicode values
       if i % 3 == 0 and i > 0:
          color = image.colorAllocate(tuple(rgb))
          image.setPixel(tuple(coords), color)
 
          coords = increment_coords(coords)
 
+      rgb[i % 3] = ord(c)
+
       i += 1
+
+   # Check to see if we stopped "mid-pixel"
+   i -= 1
+   if i % 3 != 0:
+      for j in range(1, (i % 3) + 1):
+         rgb[j * -1] = 167
+
+      color = image.colorAllocate(tuple(rgb))
+      image.setPixel(tuple(coords), color)
+
 
    return coords
 
@@ -57,15 +74,17 @@ def set_pixels_by_text(image):
 def main():
    """Initial entry point"""
 
+   # Build the temp images and fill with stop color
    image = gd.image(DIMENSIONS, True)
-   black = image.colorAllocate((  0,   0,   0))
-   white = image.colorAllocate((255, 255, 255))
-   image.fill((0, 0), white)
+   filler = image.colorAllocate((167, 167, 167))
+   image.fill((0, 0), filler)
 
+   # Write pixels to image based on input text
    coords = set_pixels_by_text(image)
 
-   smaller = gd.image((DIMENSIONS[0], coords[1]), True)
-   image.copyTo(smaller, (0, 0), (0, 0), (DIMENSIONS[0], coords[1]))
+   # Copy the temp image to a smaller image of the appropriate height
+   smaller = gd.image((DIMENSIONS[0], coords[1] + 1), True)
+   image.copyTo(smaller, (0, 0), (0, 0), (DIMENSIONS[0], coords[1] + 1))
    smaller.writePng('/home/amarriner/public_html/book.png')
 
 
--- image_to_book.py
+++ image_to_book.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python
 """Script to decrypt book image into text"""
 
+import codecs
 import gd
 import sys
 
 
 def main():
    """Initial entry point"""
-   pass
+
+   # Read book image generated from book_to_image.py
+   book = gd.image('/home/amarriner/public_html/book.png')
+
+   # Open a file for writing
+   f = codecs.open('book.txt', 'w', encoding='utf-8')
+
+   # Step through each pixel in the image (left to right and top to bottom)
+   # converting pixels into 3 utf-8 characters.
+   # Unicode value 167 is the stop character
+   for j in range(book.size()[1]):
+      for i in range(book.size()[0]):
+         rgb = book.colorComponents(book.getPixel((i, j)))
+
+         for k in range(3):
+            if rgb[k] != 167:
+               f.write(unichr(rgb[k]))
+
+
+   # Need a newline at the end of the file
+   f.write('\n')
+   f.close()
 
 
 if __name__ == '__main__':
