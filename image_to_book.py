fa85c6dfa213d19431d3fced15c61d6c1a64278e
Aaron Marriner <amarriner@amarriner.com>
2014-09-24 18:16:13 +0000

    Script to convert image to book text

---
+++ image_to_book.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Script to decrypt book image into text"""
+
+import gd
+import sys
+
+
+def main():
+   """Initial entry point"""
+   pass
+
+
+if __name__ == '__main__':
+   sys.exit(main())
